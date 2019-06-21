9dfc29fc6afd1c6d5dbd52a252fc074d97b7b7f3
MMA96 <muazamaziz2@gmail.com>
2019-06-21 17:23:01 +0500

    Update and rename uploadtime.html to uploadtime.js

--- uploadtime.js
+++ uploadtime.js
@@ -1,4 +1,3 @@
-
 function startTime() {
   var today = new Date();
   var h = today.getHours();
@@ -15,4 +14,3 @@ function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
-////
